de77f46e85f67d946fc2f709e38cabcbc61260c7
RAMOVIC239 <07ramovic@gmail.com>
2026-06-18 03:45:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-return "RAMOVIC COMMUNITY IS LIVE"
-if __name__== "main__":
-     app.run()
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/status")
+def status():
+    return render_template("status.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
